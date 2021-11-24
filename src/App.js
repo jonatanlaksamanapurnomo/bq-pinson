9d48619f9e24feeb5c89dc5038fefaa97be44ec5
jonatanlaksamanapurnomo <jonatanlaksamanapurnomo@gmail.com>
2021-11-24 20:47:12 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import dataSource from "./data.json";
+import {useEffect, useState} from "react";
+
+
+const dataTemplate = {
+    labels: [],
+    datasets: [
+        {
+            label: 'My First dataset',
+            backgroundColor: '#EC932F',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: []
+        },
+        {
+            label: 'My First dataset 2',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: []
+        }
+    ]
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [mappedData, setMappedData] = useState();
+
+    const fieldNameMapper = (item) => ({
+        label: item.app,
+        jumlah: item.jumlah
+    })
+    useEffect(() => {
+        const {data} = dataSource;
+        let result = data.map(fieldNameMapper);
+        let labels = [];
+        let jumlahArr = [];
+        result.forEach(item => {
+            labels.push(item.label);
+            jumlahArr.push(item.jumlah);
+        })
+        dataTemplate.labels = labels;
+        dataTemplate.datasets.forEach(item => {
+            item.data = jumlahArr
+        })
+        setMappedData(dataTemplate);
+    }, [])
+
+    return (
+        <div className="App">
+            <p>{JSON.stringify(dataTemplate)}</p>
+
+            <h3>Example Mapped Data Take a not the logic not already functioned</h3>
+            {JSON.stringify(mappedData)}
+        </div>
+    );
 }
 
 export default App;
